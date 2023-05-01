deb583d1b0ec63ae8a95bc344a825f0af408416e
MilitsaTsvetkova <militsa88@yahoo.com>
2023-05-01 20:48:03 +0300

    displaying platform icons

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,6 +1,6 @@
-import { Card, CardBody, Heading, Image } from "@chakra-ui/react";
-import React from "react";
+import { Card, CardBody, Heading, Image, Text } from "@chakra-ui/react";
 import { Game } from "../hooks/useGames";
+import PlatformIconList from "./PlatformIconList";
 interface GameCardProps {
   game: Game;
 }
@@ -11,6 +11,9 @@ function GameCard({ game }: GameCardProps) {
       <Image src={game.background_image}></Image>
       <CardBody>
         <Heading fontSize="2xl">{game.name}</Heading>
+        <PlatformIconList
+          platforms={game.parent_platforms.map((p) => p.platform)}
+        />
       </CardBody>
     </Card>
   );
