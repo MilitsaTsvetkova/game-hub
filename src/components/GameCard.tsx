f78a5a4c08dc23aa720c27e2b7e4966794b00511
MilitsaTsvetkova <militsa88@yahoo.com>
2023-05-04 09:56:21 +0300

    clean up game cards

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -12,13 +12,13 @@ function GameCard({ game }: GameCardProps) {
     <Card>
       <Image src={getCroppedImage(game.background_image)}></Image>
       <CardBody>
-        <Heading fontSize="2xl">{game.name}</Heading>
-        <HStack justifyContent="space-between">
+        <HStack justifyContent="space-between" marginBottom={3}>
           <PlatformIconList
             platforms={game.parent_platforms.map((p) => p.platform)}
           />
           <CreditScore score={game.metacritic} />
         </HStack>
+        <Heading fontSize="2xl">{game.name}</Heading>
       </CardBody>
     </Card>
   );
