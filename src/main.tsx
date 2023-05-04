24225f00c311c0111589e23e1d550c0f611676c1
MilitsaTsvetkova <militsa88@yahoo.com>
2023-05-04 12:13:35 +0300

    fix error in main

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
-import App from "./App.tsx";
+import App from "./App";
 import "./index.css";
 import theme from "./theme";
 
