40fcaaf32680c785f5fa1341785e14a831cf1d25
MilitsaTsvetkova <militsa88@yahoo.com>
2023-05-04 09:40:27 +0300

    implemented searching

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -3,11 +3,15 @@ import logo from "../assets/Logo/logo.webp";
 import ColorModeSwitch from "./ColorModeSwitch";
 import SearchInput from "./SearchInput";
 
-function NavBar() {
+interface NavBarProps {
+  onSearch: (searchText: string) => void;
+}
+
+function NavBar({ onSearch }: NavBarProps) {
   return (
     <HStack padding="10px">
       <Image src={logo} boxSize="60px"></Image>
-      <SearchInput />
+      <SearchInput onSearch={onSearch} />
       <ColorModeSwitch />
     </HStack>
   );
